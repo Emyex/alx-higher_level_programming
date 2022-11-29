62bef72f7bcea42164fabd8e75e74e8c1820f89f
Emyex <shinaalonge772@gmail.com>
2022-11-29 21:10:49 +0000

    doings

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if (number > 0):
+f"{number} is postive"
+elif (number < 0):
+f"{number} is negative"
+else:
+f"{number} is zero"
